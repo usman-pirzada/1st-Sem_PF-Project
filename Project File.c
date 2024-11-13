0a8d8b53e347b746faf4fe6b6632077d6cf0fa72
usman-pirzada <k243000@nu.edu.pk>
2024-11-13 15:45:51 +0500

    login function updated

--- Project File.c
+++ Project File.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
 
-int login();
+char login();
 int database();
 int report();
 int stock();
@@ -13,9 +15,35 @@ int main() {
 }
 
 // -------------------Login Function HERE--------------------
-int login() {
-    
-    return 0;
+char login(int log) {
+    char username[16], pass[21];
+    if(log == 1) {
+        printf("\nEnter Your Username: ");
+        scanf("%s15", &username);
+        printf("Enter Your Password: ");
+        scanf("%s20", &pass);
+        // strcmp after reading from file
+        /* if(true) {
+        return username;
+        } else {
+            printf("\n\tINVALID Username or Password!!");
+            if(attempt < 3) {
+                attempt++;
+                login(log);
+            } else {
+                printf("\n\tYou have entered wrong username or password 3 times!! Press any key to exit the program.");
+                ch = getch();
+                exit();
+            }
+        }*/
+    } else if(log == 2) {
+        printf("\nSet Your Username: ");
+        scanf("%s15", &username);
+        printf("Set Your Password (Length 8 to 20 digits): ");
+        scanf("%s20", &pass);   // there should be chk for pass len here
+        // now write/save username & password to firstlogin.dat file
+        // if successful written return 0, otherwise print error message & call login function again if user want otherwise exit
+    }
 }
 
 // -------------------Monthly/Yearly Report Function HERE--------------
