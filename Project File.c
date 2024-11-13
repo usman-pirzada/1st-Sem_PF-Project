886daf73c034e1cdd4c2a1ebe60984b310111f56
usman-pirzada <k243000@nu.edu.pk>
2024-11-13 16:20:31 +0500

    main & login functions updated

--- Project File.c
+++ Project File.c
@@ -8,8 +8,20 @@ int report();
 int stock();
 
 int main() {
-    printf("\tFAST Super Mart");
-    printf("\n\t==================");
+    printf("\t\tFAST Super Mart");
+    printf("\n\t\t==================");
+    // search for firstlogin.dat file & see if $FAST Super Mart$ is in its first line
+    /*
+    if(found) {
+        if(login(1) == 'Y') {   // Passing 1 for login
+            // will continue next operations
+        }
+    } else {
+        if(login(2) == 'Y') {   // Passing 2 for SignUp
+            // will continue next operations
+        }
+    }
+    */
     
     return 0;
 }
@@ -17,14 +29,14 @@ int main() {
 // -------------------Login Function HERE--------------------
 char login(int log) {
     char username[16], pass[21];
-    if(log == 1) {
+    if(log == 1) {  // 1 for login
         printf("\nEnter Your Username: ");
         scanf("%s15", &username);
         printf("Enter Your Password: ");
         scanf("%s20", &pass);
         // strcmp after reading from file
         /* if(true) {
-        return username;
+        return 'Y';
         } else {
             printf("\n\tINVALID Username or Password!!");
             if(attempt < 3) {
@@ -36,13 +48,17 @@ char login(int log) {
                 exit();
             }
         }*/
-    } else if(log == 2) {
+    } else if(log == 2) {   // 2 for SignUp
         printf("\nSet Your Username: ");
         scanf("%s15", &username);
         printf("Set Your Password (Length 8 to 20 digits): ");
         scanf("%s20", &pass);   // there should be chk for pass len here
         // now write/save username & password to firstlogin.dat file
-        // if successful written return 0, otherwise print error message & call login function again if user want otherwise exit
+        // if successful written return 'Y', otherwise print error message & call login function again if user want otherwise exit
+    } else if(log == 3) {   // 3 for Password only when performing sensitive action
+        printf("\nEnter Your Password to proceed: ");
+        scanf("%s", &pass);
+        // validation process here
     }
 }
 
