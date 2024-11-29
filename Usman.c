6b6eb6c83c45f0aee4c9f8fd7e4ecfc38e3d4540
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-30 00:11:42 +0500

    Removed old code comments

--- Usman.c
+++ Usman.c
@@ -1,90 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#define usersCount 100
-// #include <conio.h>
+#include <errno.h>      // For perror
+#include <windows.h>    // For colors and formatting
+#define WHITE "\e[0;37m"
+#define RED "\e[0;31m"
+#define BLUE "\e[0;34m"
+#define GREEN "\e[0;32m"
+#define BOLD "\e[1m"
+// #define usersCount 100
 
-void enter(int, const char *, int);    // Enter to program by your account
-int database();
-char report(int);
-int stock();
-// int takeNcount(int);
+// Menu highlighting remaining
 
 struct Account;
-struct Database;
-struct Stock;
-struct Report;
+// struct Database;
+// struct Stock;
+// struct Report;
+
+void enter(int, const char */*, int */);    // Enter to program by your account
+void menu(int);
+// int database();
+// char report(int);
+// int stock();
 
 int main() {
+    int option;
     FILE *adminFile = NULL;
     FILE *userFile = NULL;
+    system(BOLD);   // need check syntax
 
     printf("\n\t\tFAST Super Mart");
     printf("\n\t\t==================");
     adminFile = fopen("Admins.bin", "rb");
     userFile = fopen("Users.bin", "rb");
     if(adminFile == NULL && userFile == NULL) {
-        printf("\nNo Account Created Yet!! Please signUp to proceed:\n 1) Add Admin Account\n 2) Add User Account\n");
+        printf("\n" RED "No Account Created Yet!!" BLUE "Please Add an Account to Proceed:" WHITE "\n 1) Add Admin Account\n 2) Add User Account\n");
         scanf("%d", &option);
-        switch() {
+        switch(option) {
             case 1:
-                enter(1, "Admins.bin");
+                printf("\nNow SignUp below for Admin Account\n");
+                enter(2, "Admins.bin"); // Pass 2 for signup with Admins.bin file for Admin
                 system("CLS");
-                main(); // continue;    // correctit as menu loop mein chale ga ye main nhi
-                // break;
+                printf("\n\tAdmin Account Added Successfully!\n");
+                break;
             case 2:
-                enter(1, "Admins.bin");
-                main(); // continue;
+                printf("\nNow SignUp below for User Account\n");
+                enter(2, "Users.bin");  // Pass 2 for signup with Users.bin file for User
                 system("CLS");
-                // break;
+                printf("\n\tUser Account Added Successfully!\n");
+                break;
             case 3:
                 exit(0);
-                system("CLS");
-                // break;
+                break;
+            default:
+                printf("\n\tInvalid Input!! Try Again\n");
+                main(); // repeat main
+        }
+        
+    fclose(adminFile); fclose(userFile);  // As the file was opened just to verify its existance
+
+    while(1) {
+        printf("\nWhich operation do you want to perform: \n");
+        printf("\n 1) Admin Login\n 2) User Login\n 3) Add Admin\n 4) Add User\n 5) Exit\n");
+        scanf("%d", &option);
+        switch(option) {
+            case 1:
+                enter(1, "Admins.bin");   // Send 1 with "Admins.bin" for Admins' Login
+                printf("\n\tAdmin Login Successful!\n");
+                menu(1);    // Admin Menu @1
+                break;
+            case 2:
+                enter(1, "Users.bin");   // Send 1 with "Users.bin" filename for Users' Login
+                printf("\n\tUser Login Successful!\n");
+                menu(2);    // User Menu @2
+                break; 
+            case 3:
+                enter(2, "Admins.bin");   // Send 2 with "Admins.bin" for SignUp
+                printf("\n\tNew Admin Added Successfully!\n");
+                break;
+            case 4:
+                enter(2, "Users.bin");   // Send 2 with "Users.bin" filename for Users' SignUp
+                printf("\n\tNew User Added Successfully!\n");
+                break;
+
+            case 5:
+                exit(0);
+                break;
+
             default:
                 printf("\n\tInvalid Input!! Try Again\n");
                 main(); // continue;
         }
-        
-    // } else {
-    fclose(adminFile); fclose(userFile);  // File was opened just to verify its existance
-    printf("\nWhich operation do you want to perform: \n");
-    printf("\nPlease login or signUp to proceed:\n 1) Admin Login\n 2) User Login\n 3) Add Admin\n 4) Add User\n 5) Exit\n");
-    scanf("%d", &option);
-    switch(option) {
-        case 1:
-            enter(1, "Admins.bin");   // Send 1 with "Admins.bin" for Admins' Login
-            menu(1);    // Admin Menu @1
-            break;
-        case 2:
-            enter(1, "Users.bin");   // Send 1 with "Users.bin" filename for Users' Login
-            menu(2);    // User Menu @2
-            break; 
-        case 3:
-            enter(2, "Admins.bin");   // Send 2 with "Admins.bin" for SignUp
-            printf("\n\tNew Admin added successfully!\n");
-            break;
-        case 4:
-            enter(2, "Users.bin");   // Send 2 with "Users.bin" filename for Users' SignUp
-            printf("\n\tNew User added successfully!\n");
-            break;
-
-        case 5:
-            exit(0);
-            break;
-
-        default:
-            printf("\n\tInvalid Input!! Try Again\n");
-            main(); // continue;
     }
-    // }
     
     return 0;
 }
 
-// -------------------Enter Function HERE--------------------
-void enter(int log, const char *filename/*, int usersCount*/) {  // variable is global
+// -------------------Enter (Login & SignUp) Function HERE--------------------
+
+void enter(int log, const char *filename/*, int usersCount*/) {  // variable is global  // log is 1 for Login & 2 for SignUp
     struct Account *accounts = NULL;    // see for correction
     *accounts = (struct Account *) malloc(sizeof(struct Account));  // This size is enough to store SignUp data for writing to binary file
     if(accounts == NULL) {
@@ -154,7 +168,72 @@ void enter(int log, const char *filename/*, int usersCount*/) {  // variable is 
     }
 }
 
-// -------------------Monthly/Yearly Report Function HERE--------------
+// -------------------Enter (Login & SignUp) Function HERE--------------------
+
+void menu(int user) {   // 1 for Admin & 2 for User
+    int option;
+
+    if(user == 1) { // 1 for Admin
+        while(1) {
+            printf("\nWhat would you like to do now: \n");
+            printf("\n 1) Add New Item to Stock\n 2) View Stock Status\n 3) Generate Report\n 4) LogOut\n 5) LogOut & Exit Program");
+            scanf("%d", &option);
+            switch(option) {
+                case 1:
+                    // Send control to Talal's function for Adding new item to Stock
+                    menu(1);    // Admin Menu @1
+                    break;
+                case 2:
+                    // Send control to Talal's function for Viewing items of Stock
+                    break; 
+                case 3:
+                    // Send control to my function for generating report
+                    break;
+                case 4:
+                    return;     // Back to main as the user want to LogOut
+                    break;
+
+                case 5:
+                    exit(0);    // Exit Program
+                    break;
+
+                default:
+                    printf("\n\tInvalid Input!! Try Again\n");
+            }
+        }
+
+    } else if(user == 2) {  // 2 for User
+        while(1) {
+            printf("\nWhat would you like to do now: \n");
+            printf("\n 1) Add Item to Card\n 2) Remove Item from Card\n 3) Place Order\n 4) LogOut\n 5) LogOut & Exit Program");
+            scanf("%d", &option);
+            switch(option) {
+                case 1:
+                    // Send control to Mujtaba's function for Adding items to Card
+                    break;
+                case 2:
+                    // Send control to Mujtaba's function for Removing items from Card
+                    break; 
+                case 3:
+                    // Send control to Mujtaba's function to Place Order & generate receipt
+                    break;
+                case 4:
+                    return;     // Back to main as the user want to LogOut
+                    break;
+
+                case 5:
+                    exit(0);    // Exit Program
+                    break;
+
+                default:
+                    printf("\n\tInvalid Input!! Try Again\n");
+            }
+        }
+
+    }
+}
+
+// -------------------Report Generate  Function HERE--------------
 /*
 char report(int reprt) {
     FILE *freport;
@@ -187,20 +266,6 @@ char report(int reprt) {
 }
 */
 
-// ---------------Verify length anywhere for String Input------------
-int takeNcount(int limit) {
-        int count = 0;
-
-        do{
-            count++;    // Count no. of characters of Password
-        } while((username = getchar()) != '\n');
-        if(count <= limit) {
-            return 1;
-        } else {
-            return 0;
-        }
-}
-*/
 // ----------------------------Structures HERE↓----------------------
 struct Account {
     char username[20];
